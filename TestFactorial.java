ddeaf27bc92aaa811531dbe61cfd80d492dbcc53
HanishaS28 <23251a1228@gmail.com>
2026-09-18 14:42:27 +0530

    Exploring Jenkins

---
+++ TestFactorial.java
@@ -0,0 +1,9 @@
+public class TestFactorial{
+
+	public static void main(String[] args){
+
+		int n=5;
+		int res=Factorial.calculateFactorial(n);
+		System.out.println("Factorial of "+"="+ res);
+	}
+}
